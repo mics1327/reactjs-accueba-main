447b5e713a1de47efdc1f34d85f89f6cd6042d36
RexLou <cuales.166797@caloocan.sti.edu.ph>
2022-09-26 19:55:38 +0800

    added popups para sa log in

--- reactjs-accueba-main/src/App.js
+++ reactjs-accueba-main/src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import {
   Routes,
   Route,
@@ -5,11 +6,15 @@ import {
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
-
 import NavBar from "./NavBar";
+import AdminModal from "./pages/LoginPopup/AdminModal";
+import EmpModal from "./pages/LoginPopup/AdminModal";
 
 function App() {
-  return (<>
+  const [openModal, setOpenModal] = useState(false)
+  return (
+    
+  <>
     <NavBar />
     <div className="container">
       <Routes>
@@ -17,6 +22,12 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <button className='admModal'onClick={()=> setOpenModal(true)}>Admin Login</button>
+      <button className='empModal' onClick={()=> setOpenModal(true)}>Employee Login</button>
+      <AdminModal open={openModal} 
+      onClose={() => setOpenModal(false)}/>
+      <EmpModal open={openModal} 
+      onClose={() => setOpenModal(false)}/>
     </div>
   </>)
 }
