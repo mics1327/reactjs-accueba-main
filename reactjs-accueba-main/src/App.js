c9814795ec75b8de42255d59d22b87e6a57bd4c7
mics1327 <amsbdjs@gmail.com>
2022-09-26 15:56:12 +0800

    Added footer

--- reactjs-accueba-main/src/App.js
+++ reactjs-accueba-main/src/App.js
@@ -7,17 +7,22 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 
 import NavBar from "./NavBar";
+import Footer from "./Components/footer/Footer.js";
 
 function App() {
   return (<>
     <NavBar />
-    <div className="container">
+    <div className="page-container">
+      <div className="content-wrap">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
+      <Footer />
+    </div>
+
   </>)
 }
 
