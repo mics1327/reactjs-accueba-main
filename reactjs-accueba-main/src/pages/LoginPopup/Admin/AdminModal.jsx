9d8aa9756b1b19402dbd7733ce9b9d607dc3d267
RexLou <cuales.166797@caloocan.sti.edu.ph>
2022-10-02 03:46:18 +0800

    added admin-homepage

---
+++ reactjs-accueba-main/src/pages/LoginPopup/Admin/AdminModal.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const AdminModal = ({open, onClose}) => {
+    if(!open) return null
+  return (
+    <div className= 'overlay'>
+        <div className = 'modalContainer'>  
+            <div className='modalRight'>
+            <p className='closeBtn' onClick={onClose}> X </p>
+                    <div className='content'>
+                       <p>Enter username</p>
+                       <input type= "text" />
+                       <p>Enter password</p>
+                       <input type= "text" />
+                    </div>
+                   <div className="btnContainer">
+                       <button className="btnPrimary">Log In</button> 
+                   </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AdminModal;
